102af0ec78a4b9d351f95ba2b1097955b80ef9fd
Igor <iroztropinski@gmail.com>
2022-09-06 18:19:06 +0200

    Read http request headers

--- src/main/java/com/igor101/httpserver/HttpServerApp.java
+++ src/main/java/com/igor101/httpserver/HttpServerApp.java
@@ -1,10 +1,13 @@
 package com.igor101.httpserver;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,8 +19,7 @@ public class HttpServerApp {
             var connection = serverSocket.accept();
 
             var request = readRequest(connection);
-
-            System.out.println("We have a request..." + request);
+            printRequest(request);
 
             try (var os = connection.getOutputStream()) {
                 var body = """
@@ -46,7 +48,42 @@ public class HttpServerApp {
         var method = methodUrl[0];
         var url = methodUrl[1];
 
-        return new HttpReq(method, url, Map.of(), null);
+        var headers = readHeaders(r);
+
+        var body = readBody(connection.getInputStream());
+
+        return new HttpReq(method, url, headers, body);
+    }
+
+    private static Map<String, List<String>> readHeaders(BufferedReader reader) throws Exception {
+        var line = reader.readLine();
+
+        var headers = new HashMap<String, List<String>>();
+
+        while (line != null && !line.isEmpty()) {
+            var keyValue = line.split(":", 2);
+            var key = keyValue[0];
+            var value = keyValue[1];
+
+            headers.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+
+            line = reader.readLine();
+        }
+
+        return headers;
+    }
+
+    private static byte[] readBody(InputStream stream) {
+        return null;
+    }
+
+    private static void printRequest(HttpReq req) {
+        System.out.println("Method: " + req.method);
+        System.out.println("Url: " + req.url);
+        System.out.println("Headers:");
+        req.headers.forEach((k, v) -> {
+            System.out.println("%s - %s".formatted(k, v));
+        });
     }
 
     private record HttpReq(String method,
