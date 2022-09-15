334308bbf71e4a7b5622971343f39e4163e9d37d
Igor <iroztropinski@gmail.com>
2022-09-15 16:54:25 +0200

    Read http request body

--- src/main/java/com/igor101/httpserver/HttpServerApp.java
+++ src/main/java/com/igor101/httpserver/HttpServerApp.java
@@ -1,25 +1,32 @@
 package com.igor101.httpserver;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class HttpServerApp {
+
+    private static final String HTTP_NEW_LINE_SEPARATOR = "\r\n";
+    private static final String HTTP_HEAD_BODY_SEPARATOR = HTTP_NEW_LINE_SEPARATOR + HTTP_NEW_LINE_SEPARATOR;
+    private static final int HTTP_HEAD_BODY_SEPARATOR_BYTES =
+            HTTP_HEAD_BODY_SEPARATOR.getBytes(StandardCharsets.US_ASCII).length;
+    private static final int DEFAULT_PACKET_SIZE = 10_000;
+    private static final String CONTENT_LENGTH_HEADER = "content-length";
+
     public static void main(String[] args) throws Exception {
         var serverSocket = new ServerSocket(8080);
 
         while (true) {
             var connection = serverSocket.accept();
 
-            var request = readRequest(connection);
-            printRequest(request);
+            var requestOpt = readRequest(connection);
+            if (requestOpt.isEmpty()) {
+                continue;
+            }
+            printRequest(requestOpt.get());
 
             try (var os = connection.getOutputStream()) {
                 var body = """
@@ -41,40 +48,108 @@ public class HttpServerApp {
         }
     }
 
-    private static HttpReq readRequest(Socket connection) throws Exception {
-        var r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        var line = r.readLine();
+    private static Optional<HttpReq> readRequest(Socket connection) throws Exception {
+        var stream = connection.getInputStream();
+        var rawRequestHead = readRawRequestHead(stream);
+
+        if (rawRequestHead.length == 0) {
+            return Optional.empty();
+        }
+
+        var requestHead = new String(rawRequestHead, StandardCharsets.US_ASCII);
+        var lines = requestHead.split(HTTP_NEW_LINE_SEPARATOR);
+
+        var line = lines[0];
         var methodUrl = line.split(" ");
         var method = methodUrl[0];
         var url = methodUrl[1];
 
-        var headers = readHeaders(r);
+        var headers = readHeaders(lines);
 
-        var body = readBody(connection.getInputStream());
+        var bodyLength = getExpectedBodyLength(headers);
 
-        return new HttpReq(method, url, headers, body);
+        byte[] body;
+        if (bodyLength > 0) {
+            var bodyStartIndex = requestHead.indexOf(HTTP_HEAD_BODY_SEPARATOR);
+            if (bodyStartIndex > 0) {
+                var readBody = Arrays.copyOfRange(rawRequestHead,
+                        bodyStartIndex + HTTP_HEAD_BODY_SEPARATOR_BYTES,
+                        rawRequestHead.length);
+                body = readBody(stream, readBody, bodyLength);
+            } else {
+                body = new byte[0];
+            }
+        } else {
+            body = new byte[0];
+        }
+
+        return Optional.of(new HttpReq(method, url, headers, body));
     }
 
-    private static Map<String, List<String>> readHeaders(BufferedReader reader) throws Exception {
-        var line = reader.readLine();
+    private static int getExpectedBodyLength(Map<String, List<String>> headers) {
+        try {
+            return Integer.parseInt(headers.getOrDefault(CONTENT_LENGTH_HEADER, List.of("0")).get(0));
+        } catch (Exception ignored) {
+            return 0;
+        }
+    }
 
+    private static byte[] readRawRequestHead(InputStream stream) throws Exception {
+        var toRead = stream.available();
+        if (toRead == 0) {
+            toRead = DEFAULT_PACKET_SIZE;
+        }
+
+        var buffer = new byte[toRead];
+        var read = stream.read(buffer);
+        if (read <= 0) {
+            return new byte[0];
+        }
+
+        return read == toRead ? buffer : Arrays.copyOf(buffer, read);
+    }
+
+    private static Map<String, List<String>> readHeaders(String[] lines) {
         var headers = new HashMap<String, List<String>>();
 
-        while (line != null && !line.isEmpty()) {
+        for (int i = 1; i < lines.length; i++) {
+            var line = lines[i];
+            if (line.isEmpty()) {
+                break;
+            }
+
             var keyValue = line.split(":", 2);
-            var key = keyValue[0];
-            var value = keyValue[1];
+            var key = keyValue[0].toLowerCase().strip();
+            var value = keyValue[1].strip();
 
             headers.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
-
-            line = reader.readLine();
         }
 
         return headers;
     }
 
-    private static byte[] readBody(InputStream stream) {
-        return null;
+    private static byte[] readBody(InputStream stream, byte[] readBody, int expectedBodyLength) throws Exception {
+        if (readBody.length == expectedBodyLength) {
+            return readBody;
+        }
+
+        var result = new ByteArrayOutputStream(expectedBodyLength);
+        result.write(readBody);
+
+        var readBytes = readBody.length;
+        var buffer = new byte[DEFAULT_PACKET_SIZE];
+
+        while (readBytes < expectedBodyLength) {
+            var read = stream.read(buffer);
+            if (read > 0) {
+                result.write(buffer, 0, read);
+                readBytes += read;
+            } else {
+                break;
+            }
+        }
+
+        return result.toByteArray();
     }
 
     private static void printRequest(HttpReq req) {
@@ -84,6 +159,12 @@ public class HttpServerApp {
         req.headers.forEach((k, v) -> {
             System.out.println("%s - %s".formatted(k, v));
         });
+        System.out.println("Body:");
+        if (req.body.length > 0) {
+            System.out.println(new String(req.body, StandardCharsets.UTF_8));
+        } else {
+            System.out.println("Body is empty");
+        }
     }
 
     private record HttpReq(String method,
